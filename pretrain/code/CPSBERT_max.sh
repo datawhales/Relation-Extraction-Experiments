daf42f0ece9d332bb4985c8e0eb1a6153d0de282
datawhales <datawhales@gmail.com>
2021-04-28 16:57:21 +0900

    Add shell files

---
+++ pretrain/code/CPSBERT_max.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node 4  main.py \
+	--cuda 0,1,2,3 \
+        --model TS_CP_SBERT \
+	--lr 3e-5 --batch_size_per_gpu 32 --max_epoch 20 \
+	--gradient_accumulation_steps 1 \
+	--max_length 64 \
+	--save_step 5000 \
+	--alpha 0.3 \
+	--temperature 0.05 \
+	--train_sample \
+    --teacher_model ../ckpt/ckpt_exp/entity_marker/3rd_ckpt_of_step_60000 \
+	--save_dir ckpt_CPSBERT/max_pool \
+	--output_representation entity_marker \
+    --pooling_method max \
