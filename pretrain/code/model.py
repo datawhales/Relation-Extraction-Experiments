2f704ba2fa63f65681bacf04923ef791c0872403
datawhales <datawhales@gmail.com>
2021-05-03 23:21:29 +0900

    Add triple model to model.py

--- pretrain/code/model.py
+++ pretrain/code/model.py
@@ -1,10 +1,9 @@
 import os
-import pdb
 import copy
 import torch
 import torch.nn as nn
 from pytorch_metric_learning.losses import NTXentLoss
-from transformers import BertForMaskedLM, BertForPreTraining, BertTokenizer
+from transformers import BertForMaskedLM, BertTokenizer
 from sentence_transformers import SentenceTransformer
 
 def mask_tokens(inputs, tokenizer, not_mask_pos=None):
@@ -52,6 +51,207 @@ def mask_tokens(inputs, tokenizer, not_mask_pos=None):
     inputs[indices_random] = random_words[indices_random]
 
     return inputs.cuda(), labels.cuda()
+
+class TRIPLE(nn.Module):
+    """ Triple model.
+    Uses TripletMarginLoss.
+
+    Attributes:
+        model: Model to train.
+        tokenizer: Tokenizer.
+        tripletloss: TripletMarginLoss function.
+        args: args from command line.
+    """
+    def __init__(self, args):
+        super().__init__()
+        self.model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tripletloss = nn.TripletMarginLoss()
+        self.args = args
+
+    def forward(self, input, mask, label, h_pos, t_pos, h_end, t_end):
+        # input: [batch size, max len * 3]
+        # mask: [batch size, max len * 3]
+        # label, h_pos, t_pos, h_end, t_end: [batch size, 3]
+        input = input.view(-1, self.args.max_length)    # [batch size * 3, max len]
+        mask = mask.view(-1, self.args.max_length)
+        label = label.view(-1)                          # [batch_size * 3]
+        h_pos = h_pos.view(-1)
+        t_pos = t_pos.view(-1)
+        h_end = h_end.view(-1)
+        t_end = t_end.view(-1)
+
+        # mask_tokens function does not mask entity mention.
+        indice = torch.arange(0, input.size()[0])
+        not_mask_pos = torch.zeros((input.size()[0], input.size()[1]), dtype=int)
+        not_mask_pos[indice, h_pos] = 1
+        not_mask_pos[indice, t_pos] = 1
+        not_mask_pos[indice, h_end] = 1
+        not_mask_pos[indice, t_end] = 1
+
+        m_input, m_labels = mask_tokens(input.cpu(), self.tokenizer, not_mask_pos=not_mask_pos)
+        m_input = m_input.view(-1, self.args.max_length * 3)
+        m_labels = m_labels.view(-1, self.args.max_length * 3)
+
+        m_anchor_input = m_input[:, :self.args.max_length]
+        m_anchor_labels = m_labels[:, :self.args.max_length]
+        m_positive_input = m_input[:, self.args.max_length : 2 * self.args.max_length]
+        m_positive_labels = m_labels[:, self.args.max_length : 2 * self.args.max_length]
+        m_negative_input = m_input[:, 2 * self.args.max_length:]
+        m_negative_labels = m_labels[:, 2 * self.args.max_length:]
+        
+        input = input.view(-1, self.args.max_length * 3)    # [batch size, max len * 3]
+        mask = mask.view(-1, self.args.max_length * 3)
+        label = label.view(-1, 3)                           # [batch size, 3]
+        h_pos = h_pos.view(-1, 3)
+        t_pos = t_pos.view(-1, 3)
+        h_end = h_end.view(-1, 3)
+        t_end = t_end.view(-1, 3)
+
+        anchor_mask = mask[:, :self.args.max_length]
+        positive_mask = mask[:, self.args.max_length : 2 * self.args.max_length]
+        negative_mask = mask[:, 2 * self.args.max_length:]
+        
+        anchor_label = label[:, :1]
+        positive_label = label[:, 1:2]
+        negative_label = label[:, 2:]
+
+        anchor_h_pos = h_pos[:, :1]
+        positive_h_pos = h_pos[:, 1:2]
+        negative_h_pos = h_pos[:, 2:]
+
+        anchor_t_pos = t_pos[:, :1]
+        positive_t_pos = t_pos[:, 1:2]
+        negative_t_pos = t_pos[:, 2:]
+
+        anchor_h_end = h_end[:, :1]
+        positive_h_end = h_end[:, 1:2]
+        negative_h_end = h_end[:, 2:]
+
+        anchor_t_end = t_end[:, :1]
+        positive_t_end = t_end[:, 1:2]
+        negative_t_end = t_end[:, 2:]
+
+        m_anchor_outputs = self.model(input_ids=m_anchor_input, labels=m_anchor_labels, attention_mask=anchor_mask, output_hidden_states=True)
+        m_positive_outputs = self.model(input_ids=m_positive_input, labels=m_positive_labels, attention_mask=positive_mask, output_hidden_states=True)
+        m_negative_outputs = self.model(input_ids=m_negative_input, labels=m_negative_labels, attention_mask=negative_mask, output_hidden_states=True)
+        
+        mask_loss = m_anchor_outputs.loss + m_positive_outputs.loss + m_negative_outputs.loss
+        
+        anchor_last_hidden_state = m_anchor_outputs.hidden_states[-1]
+        positive_last_hidden_state = m_positive_outputs.hidden_states[-1]
+        negative_last_hidden_state = m_negative_outputs.hidden_states[-1]
+
+        indice = torch.arange(0, input.size()[0])
+
+        anchor_h_start_state = anchor_last_hidden_state[indice, anchor_h_pos]      # [batch size, hidden size]
+        anchor_h_end_state = anchor_last_hidden_state[indice, anchor_h_end]
+        anchor_t_start_state = anchor_last_hidden_state[indice, anchor_t_pos]
+        anchor_t_end_state = anchor_last_hidden_state[indice, anchor_t_end]
+
+        positive_h_start_state = positive_last_hidden_state[indice, positive_h_pos]
+        positive_h_end_state = positive_last_hidden_state[indice, positive_h_end]
+        positive_t_start_state = positive_last_hidden_state[indice, positive_t_pos]
+        positive_t_end_state = positive_last_hidden_state[indice, positive_t_end]
+
+        negative_h_start_state = negative_last_hidden_state[indice, negative_h_pos]
+        negative_h_end_state = negative_last_hidden_state[indice, negative_h_end]
+        negative_t_start_state = negative_last_hidden_state[indice, negative_t_pos]
+        negative_t_end_state = negative_last_hidden_state[indice, negative_t_end]
+
+        if self.args.output_representation == "entity_marker":
+            anchor_state = torch.cat([anchor_h_start_state, anchor_t_start_state], 1)     # [batch size, hidden size * 2]
+            positive_state = torch.cat([positive_h_start_state, positive_t_start_state], 1)
+            negative_state = torch.cat([negative_h_start_state, negative_t_start_state], 1)
+
+        elif self.args.output_representation == "all_markers":
+            anchor_h_start_state = anchor_h_start_state.unsqueeze(2)      # [batch size, hidden size, 1]
+            anchor_h_end_state = anchor_h_end_state.unsqueeze(2)
+            anchor_t_start_state = anchor_t_start_state.unsqueeze(2)
+            anchor_t_end_state = anchor_t_end_state.unsqueeze(2)
+            
+            anchor_state = torch.cat([anchor_h_start_state, anchor_h_end_state, anchor_t_start_state, anchor_t_end_state], 2)
+            
+            positive_h_start_state = positive_h_start_state.unsqueeze(2)
+            positive_h_end_state = positive_h_end_state.unsqueeze(2)
+            positive_t_start_state = positive_t_start_state.unsqueeze(2)
+            positive_t_end_state = positive_t_end_state.unsqueeze(2)
+            
+            positive_state = torch.cat([positive_h_start_state, positive_h_end_state, positive_t_start_state, positive_t_end_state], 2)
+
+            negative_h_start_state = negative_h_start_state.unsqueeze(2)
+            negative_h_end_state = negative_h_end_state.unsqueeze(2)
+            negative_t_start_state = negative_t_start_state.unsqueeze(2)
+            negative_t_end_state = negative_t_end_state.unsqueeze(2)
+            
+            negative_state = torch.cat([negative_h_start_state, negative_h_end_state, negative_t_start_state, negative_t_end_state], 2)
+
+            if self.args.pooling_method == "mean":
+                anchor_state = anchor_state.mean(dim=2)          # [batch size, hidden size]
+                positive_state = positive_state.mean(dim=2)
+                negative_state = negative_state.mean(dim=2)
+            elif self.args.pooling_method == "max":
+                anchor_state = torch.max(anchor_state, dim=2)[0]
+                positive_state = torch.max(positive_state, dim=2)[0]
+                negative_state = torch.max(negative_state, dim=2)[0]
+            elif self.args.pooling_method == "min":
+                anchor_state = torch.min(anchor_state, dim=2)[0]
+                positive_state = torch.min(positive_state, dim=2)[0]
+                negative_state = torch.min(negative_state, dim=2)[0]
+
+        elif self.args.output_representation == "end_to_first":
+            
+            anchor_h_end_state = anchor_h_end_state.unsqueeze(2)            # [batch size, hidden size, 1]
+            anchor_t_start_state = anchor_t_start_state.unsqueeze(2)
+            
+            anchor_state = torch.cat([anchor_h_end_state, anchor_t_start_state], 2)
+            
+            positive_h_end_state = positive_h_end_state.unsqueeze(2)
+            positive_t_start_state = positive_t_start_state.unsqueeze(2)
+            
+            positive_state = torch.cat([positive_h_end_state, positive_t_start_state], 2)
+
+            negative_h_end_state = negative_h_end_state.unsqueeze(2)
+            negative_t_start_state = negative_t_start_state.unsqueeze(2)
+            
+            negative_state = torch.cat([negative_h_end_state, negative_t_start_state], 2)
+
+            if self.args.pooling_method == "mean":
+                anchor_state = anchor_state.mean(dim=2)          # [batch size, hidden size]
+                positive_state = positive_state.mean(dim=2)
+                negative_state = negative_state.mean(dim=2)
+            elif self.args.pooling_method == "max":
+                anchor_state = torch.max(anchor_state, dim=2)[0]
+                positive_state = torch.max(positive_state, dim=2)[0]
+                negative_state = torch.max(negative_state, dim=2)[0]
+            elif self.args.pooling_method == "min":
+                anchor_state = torch.min(anchor_state, dim=2)[0]
+                positive_state = torch.min(positive_state, dim=2)[0]
+                negative_state = torch.min(negative_state, dim=2)[0]
+
+        elif self.args.output_representation == "all_markers_concat":
+            anchor_state = torch.cat([anchor_h_start_state, anchor_h_end_state, anchor_t_start_state, anchor_t_end_state], 1)
+            positive_state = torch.cat([positive_h_start_state, positive_h_end_state, positive_t_start_state, positive_t_end_state], 1)
+            negative_state = torch.cat([negative_h_start_state, negative_h_end_state, negative_t_start_state, negative_t_end_state], 1)
+
+        elif self.args.output_representation == "end_to_first_concat":
+            anchor_state = torch.cat([anchor_h_end_state, anchor_t_start_state], 1)
+            positive_state = torch.cat([positive_h_end_state, positive_t_start_state], 1)
+            negative_state = torch.cat([negative_h_end_state, negative_t_start_state], 1)
+
+        elif self.args.output_representation == "marker_minus":
+            anchor_state = anchor_t_start_state - anchor_h_start_state
+            positive_state = positive_t_start_state - positive_h_start_state
+            negative_state = negative_t_start_state - negative_h_start_state
+
+        else:   # CLS
+            anchor_state = anchor_last_hidden_state[:, 0, :]
+            positive_state = positive_last_hidden_state[:, 0, :]
+            negative_state = negative_last_hidden_state[:, 0, :]
+            
+        r_loss = self.tripletloss(anchor_state, positive_state, negative_state)
+
+        return r_loss
 
 class TS_CP_SBERT(nn.Module):
     """ Teacher-Student model between CP model and SBERT model.
@@ -278,7 +478,7 @@ class CP(nn.Module):
         # t_state = m_last_hidden_state[indice, t_pos]
         # state = torch.cat((h_state, t_state), 1)
 
-        h_start_state = m_last_hidden_state[indice, h_pos]
+        h_start_state = m_last_hidden_state[indice, h_pos]   # [batch size * 2, hidden size]
         h_end_state = m_last_hidden_state[indice, h_end]
         t_start_state = m_last_hidden_state[indice, t_pos]
         t_end_state = m_last_hidden_state[indice, t_end]
@@ -286,7 +486,7 @@ class CP(nn.Module):
         if self.args.output_representation == "entity_marker":
             state = torch.cat((h_start_state, t_start_state), 1)
         elif self.args.output_representation == "all_markers":
-            h_start_state = h_start_state.unsqueeze(2)
+            h_start_state = h_start_state.unsqueeze(2)      # [batch size * 2, hidden size, 1]
             h_end_state = h_end_state.unsqueeze(2)
             t_start_state = t_start_state.unsqueeze(2)
             t_end_state = t_end_state.unsqueeze(2)
