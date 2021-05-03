f7dd9f47a212dd8e4c68d850d47bbbea150bee3e
datawhales <datawhales@gmail.com>
2021-05-04 01:32:39 +0900

    Add shell files

---
+++ pretrain/code/triple_end_to_first_concat.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node 4  main.py \
+	--cuda 0,1,2,3 \
+        --model TRIPLE \
+	--lr 3e-5 --batch_size_per_gpu 32 --max_epoch 20 \
+	--gradient_accumulation_steps 1 \
+	--max_length 64 \
+	--save_step 5000 \
+	--alpha 0.3 \
+	--temperature 0.05 \
+	--train_sample \
+	--save_dir ckpt_triple/end_to_first_concat \
+	--output_representation end_to_first_concat \
